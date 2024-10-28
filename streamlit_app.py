a3c5b26f8bb4845096e6eb2fc08004150971e1f7
viraat1812 <51205823+viraat1812@users.noreply.github.com>
2024-10-28 03:11:38 +0000

    update bot

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import os
 ### set openai key, first check if it is in environment variable, if not, check if it is in streamlit secrets, if not, raise error
 
 
-st.title("Chat with Optimization Copilot")
+st.title("Chat with (convex) Optimization Copilot")
 st.write(
     "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
 )
